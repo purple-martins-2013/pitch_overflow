9d0e89de616376ac75ee9876e7d7a2d5d769fd97
DBC Apprentice <apprentice@dbc.com>
2013-08-28 11:46:45 -0700

    update typo for pitch

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 require 'faker'
 
 FactoryGirl.define do
-  factory :user do
+  factory :pitch do
     sequence(:title) { Faker::Company.bs.titleize }
     sequence(:content) { Faker::Lorem.sentences(sentence_count = 3) }
   end
