ea47e6698de7eedb128012c32c00f5b7c424cbd8
Apprentice <apprentice@dbc19.local>
2013-08-28 10:25:09 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PitchOverflow::Application.config.secret_key_base = '99aac10bd4240f24ffc9f98c531814bdcda135c31041fa2b3678a58266f81be10ca6b4f0d20b0e5cf00385389bf2effcf00c14550888336da6fd01e29ed47aab'
