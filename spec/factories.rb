544d0fd0f785ee6fe1106605aed7d926e93460de
DBC Apprentice <apprentice@dbc.com>
2013-08-28 12:14:58 -0700

    add reaction factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -6,4 +6,8 @@ FactoryGirl.define do
     sequence(:content) { Faker::Lorem.sentences(sentence_count = 3).join(' ') }
   end
 
+  factory :reaction do
+    sequence(:content) { Faker::Lorem.sentence(sentence_count = 2).join(' ')}
+  end
+  
 end
