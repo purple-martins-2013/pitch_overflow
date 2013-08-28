e8295b193fbac4fe6f39e3821c834c6e024176ab
DBC Apprentice <apprentice@dbc.com>
2013-08-28 12:00:01 -0700

    add failing test for response-pitch relationship

--- spec/models/reaction_spec.rb
+++ spec/models/reaction_spec.rb
@@ -2,14 +2,17 @@ require 'spec_helper'
 
 describe Reaction do
 
+  let(:pitch) { FactoryGirl.create(:pitch) }
+
   before do
-    @reaction = Reaction.new(content: 'Example content', score: 2)
+    @reaction = pitch.reactions.build(content: 'Example content', score: 2)
   end
 
   subject {@reaction}
 
   it {should respond_to(:content)}
   it {should respond_to(:score)}
+  it {should respond_to(:pitch_id)}
 
   context "when content is blank" do
     before { @reaction.content = nil }
@@ -20,6 +23,12 @@ describe Reaction do
     it 'should have a default score of zero' do
       reaction = Reaction.create(content: 'fofo')
       expect(reaction.score).to eq 0
+    end
+  end
+
+  describe "pitch relationship" do
+    it 'should be assigned to a pitch' do
+      expect(@reaction.pitch).to eq pitch
     end
   end
 
