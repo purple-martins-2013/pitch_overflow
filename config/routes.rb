b26498c270c5c22a8a1edaf20ddd6f8543e03251
Apprentice <apprentice@dbc24.local>
2013-08-28 10:49:01 -0700

    Nest resources - pitches have many reactions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 PitchOverflow::Application.routes.draw do
+
+  resources :pitches do
+    resources :reactions
+  end
+
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
