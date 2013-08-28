924161089a283dff7bee7ca67bafc7d4735990e8
DBC Apprentice <apprentice@dbc.com>
2013-08-28 12:07:46 -0700

    fix factory (:content is now a string)

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,7 +3,7 @@ require 'faker'
 FactoryGirl.define do
   factory :pitch do
     sequence(:title) { Faker::Company.bs.titleize }
-    sequence(:content) { Faker::Lorem.sentences(sentence_count = 3) }
+    sequence(:content) { Faker::Lorem.sentences(sentence_count = 3).join(' ') }
   end
 
 end
