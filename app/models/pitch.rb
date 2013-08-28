ba96d46abe949755faf3ed0e44bc5fc0f045205e
Apprentice <apprentice@dbc24.local>
2013-08-28 12:31:39 -0700

    implement validations for presences of title and content. create methods for upvote, downvote, and titleize functionality

--- app/models/pitch.rb
+++ app/models/pitch.rb
@@ -1,3 +1,26 @@
 class Pitch < ActiveRecord::Base
+
+  attr_reader :title, :content, :score
+
   has_many :reactions
+  validates_presence_of :title
+  validates_presence_of :content
+
+  before_save :titleize_title
+
+  def upvote!
+    self.score += 1
+    self.save
+  end
+
+  def downvote!
+    self.score -= 1
+    self.save
+  end
+
+  private
+
+  def titleize_title
+    self.title = title.titleize
+  end
 end
