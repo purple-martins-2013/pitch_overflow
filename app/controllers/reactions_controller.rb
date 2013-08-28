290513fcfcc6c01532a7f1cd3780190ac2e6f8e5
DBC Apprentice <apprentice@dbc.com>
2013-08-28 15:06:27 -0700

    create new reaction action

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -1,7 +1,15 @@
 class ReactionsController < ApplicationController  
+  
   def create
     pitch = Pitch.find(params[:pitch_id])
+    pitch.reactions.create(reaction_params)
     redirect_to pitch_path(pitch)
+  end
+  
+  private
+
+  def reaction_params
+    params.require(:reaction).permit(:content)
   end
 
 end
--- spec/controllers/reactions_controller_spec.rb
+++ spec/controllers/reactions_controller_spec.rb
@@ -3,14 +3,32 @@ require 'spec_helper'
 
 describe ReactionsController do
   let(:pitch) { FactoryGirl.create(:pitch) }
-  let(:reaction) { pitch.reaction.build(content: 'example')}
+  let(:reaction) { pitch.reactions.build(content: 'example')}
 
   describe '#create' do
     context "when I try to create a reaction with empty params" do
+      it "should raise an error" do
+        expect{ post :create, { pitch_id: pitch.id, reaction: { } } }.to raise_error
+      end
+    end
+
+    context "when I try to create a reaction with complete params" do
+      
+      before do
+        post :create, { pitch_id: pitch.id, reaction: {
+            content: reaction.content
+          }
+        }
+      end
+
       it "should redirect to the pitch page" do
-        post :create, pitch_id: pitch.id
         expect(response).to redirect_to(pitch_path(pitch))
       end
+
+      it "should create a new reaction" do
+        expect(Reaction.last.content).to eq reaction.content
+      end
+
     end
   end
 end
