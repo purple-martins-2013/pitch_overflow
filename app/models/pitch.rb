7c1a5e3a8563cee73c8611cbbeae009d3d3316a1
Apprentice <apprentice@devbootcamp.com>
2013-08-29 14:44:15 -0700

    add vote model and testing

--- app/models/pitch.rb
+++ app/models/pitch.rb
@@ -3,6 +3,7 @@ class Pitch < ActiveRecord::Base
   before_save :titleize_title
 
   has_many :reactions
+  has_many :votes
   belongs_to :user
   validates_presence_of :title
   validates_presence_of :content
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
 
   has_many :pitches
   has_many :reactions
+  has_many :votes
 
   validates_presence_of :username, :provider, :uid
 
