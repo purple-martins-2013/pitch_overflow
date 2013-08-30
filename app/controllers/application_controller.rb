934f1325c80565e5346dc87c88730248576e5f8e
DBC Apprentice <apprentice@dbc.com>
2013-08-30 15:35:44 -0700

    Redirect to omniauth authentication if voting without logging in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :require_login, only: [:create]
+  before_action :require_login, only: [:create, :upvote, :downvote]
 
   private
 
--- spec/controllers/pitches_controller_spec.rb
+++ spec/controllers/pitches_controller_spec.rb
@@ -61,13 +61,86 @@ describe PitchesController do
         post :create, { pitch: {title: "My amazing idea", content: "It will be uber successful" }}
       end
 
-      it "should redirect to the omniauth authorization page" do
+      it "should redirect to the omniauth authentication page" do
         post_action
         expect(response).to redirect_to '/auth/github'
       end
 
       it "should not create a pitch" do
         expect { post_action }.not_to change { Pitch.count }
+      end
+    end
+  end
+
+  describe "pitches#upvote" do
+
+    before { pitch }
+
+    let(:post_action) do
+      post :upvote, { id: pitch.id }
+    end
+
+    context "when I'm logged in" do
+      include LoginHelper
+      before(:each) do
+        @user = create(:user)
+        login(@user)
+      end
+
+      it "creates a new vote" do
+        expect { post_action }.to change { Vote.count }
+      end
+
+      it 'should increase the score by 1' do
+        expect { post_action }.to change { Pitch.last.score }.by(1)
+      end
+
+    end
+
+    context "when I'm not logged in" do
+      it "does not create a new vote" do
+        expect { post_action }.not_to change { Vote.count }
+      end
+
+      it "redirects to omniauth authentication page" do
+        post_action
+        expect(response).to redirect_to '/auth/github'
+      end
+    end
+  end
+
+  describe "pitches#downvote" do
+
+    before { pitch }
+
+    let(:post_action) do
+      post :downvote, { id: pitch.id }
+    end
+
+    context "when I'm logged in" do
+      include LoginHelper
+      before(:each) do
+        @user = create(:user)
+        login(@user)
+      end
+
+      it "creates a new vote" do
+        expect { post_action }.to change { Vote.count }
+      end
+
+      it 'should decrease the score by 1' do
+        expect { post_action }.to change { Pitch.last.score }.by(-1)
+      end
+    end
+
+    context "when I'm not logged in" do
+      it "does not create a new vote" do
+        expect { post_action }.not_to change { Vote.count }
+      end
+
+      it "redirects to omniauth authentication page" do
+        post_action
+        expect(response).to redirect_to '/auth/github'
       end
     end
   end
