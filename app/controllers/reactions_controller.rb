6d85eb459c7e29fe59181f1ad752a8c03ce019db
DBC Apprentice <apprentice@dbc.com>
2013-08-30 14:30:49 -0700

    Redirect to github login if user tries to create reaction without logging in

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -1,5 +1,7 @@
 class ReactionsController < ApplicationController  
-  
+
+  before_action :require_login, only: [:create]
+
   def create
     pitch = Pitch.find(params[:pitch_id])
     pitch.reactions.create(reaction_params)
@@ -7,6 +9,12 @@ class ReactionsController < ApplicationController
   end
   
   private
+
+  def require_login
+    unless current_user
+      redirect_to '/auth/github'
+    end
+  end
 
   def reaction_params
     params.require(:reaction).permit(:content)
--- spec/controllers/reactions_controller_spec.rb
+++ spec/controllers/reactions_controller_spec.rb
@@ -6,37 +6,67 @@ describe ReactionsController do
   let(:reaction) { pitch.reactions.build(content: 'example')}
 
   describe '#create' do
-    context "when I try to create a reaction with empty params" do
-      it "should raise an error" do
-        expect{ post :create, { pitch_id: pitch.id, reaction: { } } }.to raise_error
+
+    context "when I'm logged in" do
+      include LoginHelper
+      before do
+        @user = create(:user)
+        login(@user)
+      end
+
+      describe "when I try to create a reaction with empty params" do
+        it "should raise an error" do
+          expect{ post :create, { pitch_id: pitch.id, reaction: { } } }.to raise_error
+        end
+      end
+
+      describe "when I try to create a reaction with complete params" do
+        
+        before do
+          post :create, { pitch_id: pitch.id, reaction: {
+              content: reaction.content
+            }
+          }
+        end
+
+        it "should redirect to the show pitch page" do
+          expect(response).to redirect_to(pitch_path(pitch))
+        end
+
+        it "should create a new reaction" do
+          expect(Reaction.last.content).to eq reaction.content
+        end
+
+        it "should not accept a :score parameter" do
+          post :create, { pitch_id: pitch.id, reaction: {
+              content: reaction.content, score: 10000000
+            }
+          }
+          expect(Reaction.last.score).to eq 0
+        end
       end
     end
 
-    context "when I try to create a reaction with complete params" do
-      
-      before do
-        post :create, { pitch_id: pitch.id, reaction: {
-            content: reaction.content
+    context "when I'm not logged in" do
+
+      describe "when I try to create a reaction" do
+
+       let(:post_action) do
+          post :create, { pitch_id: pitch.id, reaction: {
+              content: reaction.content
+            }
           }
-        }
-      end
+        end
 
-      it "should redirect to the show pitch page" do
-        expect(response).to redirect_to(pitch_path(pitch))
-      end
+        it "should redirect to the omniauth authorization page" do
+          post_action
+          expect(response).to redirect_to '/auth/github'
+        end
 
-      it "should create a new reaction" do
-        expect(Reaction.last.content).to eq reaction.content
+        it "should not create a reaction" do
+          expect { post_action }.not_to change { Reaction.count }
+        end
       end
-
-      it "should not accept a :score parameter" do
-        post :create, { pitch_id: pitch.id, reaction: {
-            content: reaction.content, score: 10000000
-          }
-        }
-        expect(Reaction.last.score).to eq 0
-      end
-
     end
   end
 end
