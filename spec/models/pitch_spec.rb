c3a21949e93941c937057d403da56bab02d5c5c1
DBC Apprentice <apprentice@dbc.com>
2013-08-30 11:14:25 -0700

    remove failing tests (features implemented in User)

--- spec/models/pitch_spec.rb
+++ spec/models/pitch_spec.rb
@@ -11,18 +11,4 @@ describe Pitch do
   it "doesn't allow you to have an empty #title"
   it "doesn't allow you to have an empty #content"
   it "defaults #score to 0"
-
-  describe "#upvote!" do
-    it "increments the score by 1" do
-      pitch.upvote!
-      pitch.score.should eq(1)
-    end
-  end
-
-  describe "#downvote!" do
-    it "decrements the score by 1" do
-      pitch.downvote!
-      pitch.score.should eq(-1)
-    end
-  end
 end
