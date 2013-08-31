e89ffc53db3b364d21ea560d9cabb1eea9391008
Ishwar Nagar <ishwar.nagar@gmail.com>
2013-08-31 15:40:26 -0700

    call AddUpVoteWidget in show.js, resolve merge conflict, and remove redundant file

--- app/assets/javascripts/pitches/show.js
+++ app/assets/javascripts/pitches/show.js
@@ -1,7 +1,4 @@
 $(document).ready(function() {
   new AddReactionWidget('#new_reaction', new ReactionsContainer('#pitch_reactions'));
-<<<<<<< HEAD
-=======
   new AddUpVoteWidget('#upvote', new ScoreView('#score1'));
->>>>>>> upvote-is-created-with-AJAX
 });
