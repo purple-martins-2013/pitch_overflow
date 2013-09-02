4c677a286186526b898df6d3eeac4a3d37c4a3b6
Zee Spencer <zee@zeespencer.com>
2013-09-02 16:41:56 -0700

    Use capybara login only if we can't access request.env

--- spec/support/login_helper.rb
+++ spec/support/login_helper.rb
@@ -1,7 +1,7 @@
 module LoginHelper
   # For more info on testing omniauth without faking: https://github.com/intridea/omniauth/wiki/Integration-Testing
   def login(user)
-    if page && page.respond_to(:set_rack_session)
+    if !request && page && page.respond_to?(:set_rack_session)
       page.set_rack_session(user_id: user.id)
     else
       request.env["rack.session"][:user_id] = user.id
