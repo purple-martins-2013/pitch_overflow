ae228ff42a219414c4fda5e852381ece1ac07087
Ishwar Nagar <ishwar.nagar@gmail.com>
2013-09-04 20:46:02 -0700

    remove keys from omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  #provider :github, ENV['github_id'], ENV['github_secret']
-  provider :github, '70a5ef133d8a8bc04375', '17c2065e516bc48cea8d42a1f9b5bf50f80858c3'
+  provider :github, ENV['github_id'], ENV['github_secret']
 end
